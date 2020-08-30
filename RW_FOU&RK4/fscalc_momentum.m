8a58ff457d0200207fb28fc687ae5d5a1fb1bf55
MGYBY <53412286+MGYBY@users.noreply.github.com>
2020-08-30 15:24:31 -0400

    Add files via upload

--- RW_FOU&RK4/fscalc_momentum.m
+++ RW_FOU&RK4/fscalc_momentum.m
@@ -8,17 +8,26 @@ u(1) = un*hn/h(1);
 uhat(1) = u(1);
 % radiating B.C.
 q(ind2(end)+1) = q(ind2(end));
+q(ind2(end)+2) = q(ind2(end));
 u(ind2(end)+1) = u(ind2(end));
+u(ind2(end)+2) = u(ind2(end));
 
-for k = ind2
-    if (u(k) > 0)
-        uhat(k) = u(k);
+for k = ind2(2):ind2(end)
+ ua = (u(k)+u(k+1))/2;
+    if (ua >= 0)
+        phiu = u(k-1);
+        phic = u(k);
+        phid = u(k+1);
     else
-        uhat(k) = u(k + 1);
+        phiu = u(k+2);
+        phic = u(k+1);
+        phid = u(k);
     end
+    uhat(k) = FluxLim_MINMOD(phiu, phic, phid);
 end
 
 qu = uhat.^2.* h;
+qu(ind2(1)) = u(ind2(1))^2*h(ind2(1)); % second cell upwind scheme
 frictionterm = cf * u .^ 2 ./ 2;
 fs2 = -g*(1/dx)*hhat(ind2).*(h(ind2)-h(ind2-1))-(qu(ind2)-qu(ind2-1))/ dx...
     -frictionterm(ind2)+g*tan_theta*hhat(ind2);
