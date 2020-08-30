cea38c9ec5b823b517b8dc10d80875095b9eda74
MGYBY <53412286+MGYBY@users.noreply.github.com>
2020-08-30 15:24:15 -0400

    Add files via upload

---
+++ RW_MINMOD_RK4/fscalc_continuity.m
@@ -0,0 +1,11 @@
+function [ fs1 ] = fscalc_continuity( ind1, q, dx )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+% guarantee the radiating B.C.
+q(ind1(end)+1) = q(ind1(end));
+q(ind1(end)+2) = q(ind1(end));
+
+fs1 = -1/dx*(q(ind1+1)-q(ind1));
+
+end
+
